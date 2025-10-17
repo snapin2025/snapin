2340542003c69bb1c69f52d1159ea6b4879ca977
LavrishinA <snapin2025@gmail.com>
2025-10-17 16:22:28 +0300

    init: init commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+import {ReactNode} from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body >
+        {children}
+      </body>
+    </html>
+  );
+}
