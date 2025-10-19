d237406166fc2c8e04a193bf989e95753ead3bc3
LavrishinA <snapin2025@gmail.com>
2025-10-19 19:38:23 +0300

    refactor: set semi false, adjust eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,38 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook'
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import query from '@tanstack/eslint-plugin-query';
-import prettier from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import query from '@tanstack/eslint-plugin-query'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript'), {
-  plugins: {
-    '@tanstack/query': query,
-    prettier,
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      '@tanstack/query': query,
+      prettier,
+    },
+    rules: {
+      // Включаем рекомендуемые правила
+      ...query.configs.recommended.rules,
+      'prettier/prettier': 'warn',
+    },
   },
-  rules: {
-    // Включаем рекомендуемые правила
-    ...query.configs.recommended.rules,
-    'prettier/prettier': 'warn',
+    ...prettierConfig.rules,
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
-}, prettierConfig, {
-  ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
-}, ...storybook.configs["flat/recommended"]];
+  ...storybook.configs['flat/recommended'],
+]
 
-export default eslintConfig;
+export default eslintConfig
