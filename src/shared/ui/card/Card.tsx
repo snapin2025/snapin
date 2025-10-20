c4ec11f42f8de754ae68c65e93812a096009ebca
LavrishinA <snapin2025@gmail.com>
2025-10-20 03:09:59 +0300

    feat: add Card polymorphic component

---
+++ src/shared/ui/card/Card.tsx
@@ -0,0 +1,21 @@
+import { ComponentPropsWithoutRef, ElementType, PropsWithChildren } from 'react'
+import { clsx } from 'clsx'
+import s from './card.module.css'
+
+type CardProps<T extends ElementType> = {
+  as?: T
+}
+
+type Props<T extends ElementType> = PropsWithChildren<CardProps<T>> & ComponentPropsWithoutRef<T>
+
+export const Card = <T extends ElementType = 'div'>({ as, children, className, ...restPtops }: Props<T>) => {
+  const Component = as || 'div'
+
+  return (
+    <Component className={clsx(s.card, className)} {...restPtops}>
+      {children}
+    </Component>
+  )
+}
+
+
