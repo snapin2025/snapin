7976235ae0da95f4ccec81752c3d8d12210f8a7d
vzw83 <vzw83@yandex.ru>
2025-10-21 09:12:48 +0300

    NEXT-5 feat: add stories

---
+++ src/shared/ui/button/Button.tsx
@@ -0,0 +1,33 @@
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import s from './button.module.css';
+import clsx from 'clsx';
+
+type Props = {
+  variant?: 'primary' | 'secondary' | 'outlined' | 'textButton';
+  asChild?: boolean;
+} & ComponentPropsWithoutRef<'button'>;
+
+export const Button = (props: Props) => {
+  const { variant = 'primary', asChild, className, ...rest } = props;
+
+  const Component = asChild ? Slot : 'button';
+
+  return <Component {...rest} className={clsx(s.button, s[variant], className)} />;
+};
+
+// export const Button = forwardRef<HTMLButtonElement, Props>((props, forwardedRef) => {
+//   const { variant = 'primary', asChild, className, ...rest } = props;
+//
+//   const Component = asChild ? Slot : 'button';
+//
+//   return <Component {...rest} ref={forwardedRef} className={clsx(s.button, s[variant], className)} />;
+// });
+
+Button.displayName = 'Button';
+
+/*
+asChild
+clonElement
+slot in Radix
+*/
---
+++ src/shared/ui/button/button.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import { Button } from '@/shared/ui/button/Button';
+
+const meta = {
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Button',
+    disabled: false,
+    variant: 'primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    children: 'Button',
+    disabled: false,
+    variant: 'secondary',
+  },
+};
+
+export const Outlened: Story = {
+  args: {
+    children: 'Button',
+    disabled: false,
+    variant: 'outlined',
+  },
+};
+
+export const Text: Story = {
+  args: {
+    children: 'Button',
+    disabled: false,
+    variant: 'textButton',
+  },
+};
+
+export const LinkAsButton: Story = {
+  args: {
+    asChild: true,
+    children: <a href="#">Link</a>,
+    disabled: false,
+    variant: 'primary',
+  },
+};
