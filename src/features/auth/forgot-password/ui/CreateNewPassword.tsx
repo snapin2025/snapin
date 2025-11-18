aaa398a1d9b3966e592c0a7f83497c2b61778f9f
Viktoria-Inctagram <vchunik@mail.ru>
2025-11-18 14:18:46 +0300

    fix: Storybook forCreateNewPassword

--- src/features/auth/forgot-password/ui/CreateNewPassword.tsx
+++ src/features/auth/forgot-password/ui/CreateNewPassword.tsx
@@ -10,7 +10,7 @@ import { useSearchParams } from 'next/navigation'
 import { useRouter } from 'next/navigation'
 
 export const CreateNewPassword = () => {
-  // const router = useRouter() // ← создаём router
+  const router = useRouter() // ← создаём router
   const searchParams = useSearchParams() // ← ДОБАВЛЕНО: получаем параметры из URL
   const recoveryCode = searchParams?.get('code') || '' // ← ДОБАВЛЕНО: реальный код из ссылки письма
 
@@ -43,8 +43,8 @@ export const CreateNewPassword = () => {
       },
       {
         onSuccess: () => {
-          console.log('/sign-in')
-          // router.push('/sign-in') // ← РЕДИРЕКТ ПО ТЗ ШАГ 12
+          // console.log('/sign-in')
+          router.push('/sign-in') // ← РЕДИРЕКТ ПО ТЗ ШАГ 12
           reset()
         }
       }
