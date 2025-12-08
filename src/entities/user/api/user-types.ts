58f9a92a975e9a70325b124a1d54df5152ed9d63
vzw83 <vzw83@yandex.ru>
2025-12-08 11:22:18 +0300

    Next-41: fix CreatePostDialog (CroppingStep, PublicationStep) and add new types

--- src/entities/user/api/user-types.ts
+++ src/entities/user/api/user-types.ts
@@ -90,3 +90,47 @@ export type EmailResendingErrorResponse = {
 export type TotalCountUsersResponse = {
   totalCount: number
 }
+export type CreatePostPayload = {
+  description: string
+  childrenMetadata: {
+    uploadId: string
+  }[]
+}
+export type CreatePostResponse = {
+  id: number
+  userName: string
+  description: string
+  location: string
+  images: {
+    url: string
+    width: number
+    height: number
+    fileSize: number
+    createdAt: string
+    uploadId: string
+  }[]
+  createdAt: string
+  updatedAt: string
+  ownerId: number
+  avatarOwner: string
+  owner: {
+    firstName: string
+    lastName: string
+  }
+  likesCount: number
+  isLiked: boolean
+  avatarWhoLikes: boolean
+}
+export type PostImagesPayload = {
+  file: File[]
+}
+export type PostImagesResponse = {
+  images: {
+    url: string
+    width: number
+    height: number
+    fileSize: number
+    createdAt: string
+    uploadId: string
+  }[]
+}
