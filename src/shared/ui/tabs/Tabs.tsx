5e074dec401a2c23dc1f20615295ee1d6d13ebc2
LavrishinA <snapin2025@gmail.com>
2025-12-16 09:02:36 +0300

    feat: add Tabs component

---
+++ src/shared/ui/tabs/Tabs.tsx
@@ -0,0 +1,29 @@
+import * as TabsPrimitive from '@radix-ui/react-tabs'
+import { ComponentPropsWithoutRef, ComponentRef, Ref } from 'react'
+import { Typography } from '@/shared/ui'
+import s from './Tabs.module.css'
+import clsx from 'clsx'
+
+type Props = {} & ComponentPropsWithoutRef<typeof TabsPrimitive.Root> & {
+    ref?: Ref<ComponentRef<typeof TabsPrimitive.Root>>
+  }
+
+type TabsTriggerProps = ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger> & {
+  ref?: Ref<ComponentRef<typeof TabsPrimitive.Trigger>>
+}
+
+export const Tabs = (props: Props) => {
+  const { ref, ...rest } = props
+  return <TabsPrimitive.Root ref={ref} {...rest} />
+}
+
+export const TabsTrigger = ({ className, ref, children, ...rest }: TabsTriggerProps) => {
+  return (
+    <TabsPrimitive.Trigger className={clsx(className, s.trigger)} {...rest} ref={ref}>
+      <Typography variant={'h3'}>{children}</Typography>
+    </TabsPrimitive.Trigger>
+  )
+}
+
+export const TabsContent = TabsPrimitive.Content
+export const TabsList = TabsPrimitive.List
