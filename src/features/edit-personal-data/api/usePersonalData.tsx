eb7cf5c311b2c905a898ecce3a27d71560cf8fcd
LavrishinA <snapin2025@gmail.com>
2025-12-25 00:32:10 +0300

    ref: input date com make single mode

--- src/features/edit-personal-data/api/usePersonalData.tsx
+++ src/features/edit-personal-data/api/usePersonalData.tsx
@@ -2,10 +2,16 @@ import { AxiosError } from 'axios'
 
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { PersonalData, userApi } from '@/entities/user'
+import { format, parseISO } from 'date-fns'
+import { DATE_FORMAT } from '../model/lib/consts'
 
 export const usePersonalData = () => {
   return useSuspenseQuery<PersonalData, AxiosError<{ statusCode: number }>>({
     queryKey: ['personal-data'],
-    queryFn: userApi.getPersonalData
+    queryFn: userApi.getPersonalData,
+    select: (data: PersonalData) => ({
+      ...data,
+      dateOfBirth: data.dateOfBirth ? format(parseISO(data.dateOfBirth), DATE_FORMAT) : ''
+    })
   })
 }
