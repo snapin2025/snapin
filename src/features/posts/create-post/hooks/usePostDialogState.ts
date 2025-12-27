6632a1017a3a437350a0cfd5dffa04eeb64af19e
vzw83 <vzw83@yandex.ru>
2025-12-27 10:45:19 +0300

    Next-54: fix CreatePostDialog

--- src/features/posts/create-post/hooks/usePostDialogState.ts
+++ src/features/posts/create-post/hooks/usePostDialogState.ts
@@ -6,20 +6,23 @@ export const usePostDialogState = (open: boolean) => {
   const [images, setImages] = useState<ImageItem[]>([])
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
   const imagesRef = useRef<ImageItem[]>([])
-
+  console.log(open)
   // Синхронизируем ref с state
   useEffect(() => {
-    imagesRef.current = images
+    imagesRef.current = images //4
   }, [images])
 
   useEffect(() => {
     if (!open) {
       // Используем ref для получения актуальных images без добавления в зависимости
-      const currentImages = imagesRef.current
+
+      const currentImages = imagesRef.current //1
+
       currentImages.forEach((img) => {
         URL.revokeObjectURL(img.originalUrl)
         if (img.croppedUrl) URL.revokeObjectURL(img.croppedUrl)
       })
+
       setImages([])
       setStep('select')
       setCurrentImageIndex(0)
