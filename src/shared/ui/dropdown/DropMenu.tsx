6784a0dd87eebf38ceb5412b0094389ab6e8207e
Viktoria-Inctagram <vchunik@mail.ru>
2026-03-03 11:29:15 +0300

    fix: drowmenu add   isFollowing = false

--- src/shared/ui/dropdown/DropMenu.tsx
+++ src/shared/ui/dropdown/DropMenu.tsx
@@ -1,113 +1,3 @@
-// 'use client'
-//
-// import { useState } from 'react'
-// import { Dropdown } from './Dropdown'
-// import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
-// import s from './Dropdown.module.css'
-// import { DeleteIcon, DotsIcon, EditIcon, FollowIcon, UnfollowIcon } from '@/shared/ui'
-// import { useAuth } from '@/shared/lib'
-//
-// type DropMenuProps = {
-//   onEdit?: () => void
-//   onDelete?: () => void
-//   onFollow?: () => void
-//   onUnfollow?: () => void
-//   onCopyLink?: () => void
-//   ownerId: number
-//   currentUserId?: number | null
-//   isFollowing?: boolean | null
-//   isFollowPending?: boolean
-// }
-//
-// export const DropMenu = ({
-//   onEdit,
-//   onDelete,
-//   onFollow,
-//   onUnfollow,
-//   ownerId,
-//   currentUserId,
-//   isFollowing = null,
-//   isFollowPending = false
-// }: DropMenuProps) => {
-//   const [open, setOpen] = useState(false)
-//
-//   const { user } = useAuth()
-//
-//   // Показываем Edit/Delete только если это пост текущего пользователя
-//   const canEdit = !!currentUserId && currentUserId === ownerId
-//   // Показываем Follow/Unfollow только если пользователь авторизован и это не его пост
-//   const canFollow = !!user && !!currentUserId && currentUserId !== ownerId
-//
-//   const myTrigger = (
-//     // обертка
-//
-//     <button type="button" className={s.IconButton} aria-label="Post options">
-//       {/*три точки*/}
-//       <DotsIcon className={s.dots} />
-//     </button>
-//   )
-//
-//   return (
-//     <Dropdown trigger={myTrigger} align="end" open={open} onOpenChange={setOpen}>
-//       {canEdit && (
-//         <>
-//           <DropdownMenu.Item
-//             className={s.DropdownMenuItem}
-//             onSelect={(event) => {
-//               event.preventDefault()
-//               setOpen(false)
-//               onEdit?.()
-//             }}
-//           >
-//             <EditIcon className={s.icon} />
-//             Edit Post
-//           </DropdownMenu.Item>
-//
-//           <DropdownMenu.Item
-//             className={s.DropdownMenuItem}
-//             onSelect={(event) => {
-//               event.preventDefault()
-//               setOpen(false)
-//               onDelete?.()
-//             }}
-//           >
-//             <DeleteIcon className={s.icon} />
-//             Delete Post
-//           </DropdownMenu.Item>
-//         </>
-//       )}
-//
-//       {canFollow && (
-//         <>
-//           <DropdownMenu.Item
-//             className={s.DropdownMenuItem}
-//             disabled={isFollowPending || isFollowing === null}
-//             onSelect={(event) => {
-//               event.preventDefault()
-//               setOpen(false)
-//               if (isFollowing === null) {
-//                 return
-//               }
-//
-//               if (isFollowing) {
-//                 onUnfollow?.()
-//                 return
-//               }
-//
-//               onFollow?.()
-//             }}
-//           >
-//             {isFollowing ? <UnfollowIcon className={s.icon} /> : <FollowIcon className={s.icon} />}
-//             {isFollowing === null ? 'Loading...' : isFollowing ? 'Unfollow' : 'Follow'}
-//           </DropdownMenu.Item>
-//         </>
-//       )}
-//     </Dropdown>
-//   )
-// }
-//
-// export default DropMenu
-
 'use client'
 
 import { useState } from 'react'
@@ -125,7 +15,7 @@ type DropMenuProps = {
   onCopyLink?: () => void
   ownerId: number
   currentUserId?: number | null
-  isFollowing?: boolean // Добавила длямоего файла
+  isFollowing?: boolean
 }
 
 export const DropMenu = ({
@@ -136,7 +26,7 @@ export const DropMenu = ({
   onCopyLink,
   ownerId,
   currentUserId,
-  isFollowing
+  isFollowing = false
 }: DropMenuProps) => {
   const [open, setOpen] = useState(false)
 
